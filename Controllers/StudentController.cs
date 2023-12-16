96c1214b9e89790a3913b4128e89a6625090722f
Bernard Amaeme <bernardamaeme@gmail.com>
2023-12-16 05:04:13 +0100

    Add project files.

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,95 @@
+﻿using FireSharp.Config;
+using FireSharp.Interfaces;
+using FireSharp.Response;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using StudentInformation.Models;
+
+namespace StudentInformation.Controllers
+{
+    public class StudentController : Controller
+    {
+        IFirebaseConfig config = new FirebaseConfig
+        {
+            AuthSecret = "mlWlpfmHeht4yxJNJ8lOjX5kSB2XYbWFCIMJU9xu",
+            BasePath = "https://studentcrudapp-dccea-default-rtdb.firebaseio.com"
+        };
+        IFirebaseClient client;
+
+        public IActionResult Index()
+        {
+            client = new FireSharp.FirebaseClient(config);
+            FirebaseResponse response = client.Get("Students");
+            dynamic data = JsonConvert.DeserializeObject<dynamic>(response.Body);
+            var list = new List<Student>();
+            if (data != null)
+            {
+                foreach (var item in data)
+                {
+                    list.Add(JsonConvert.DeserializeObject<Student>(((JProperty)item).Value.ToString()));
+                }
+            }
+            return View(list);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Student student)
+        {
+            try
+            {
+                client = new FireSharp.FirebaseClient(config);
+                var data = student;
+                PushResponse response = client.Push("Students/", data);
+                data.Id = response.Result.name;
+                SetResponse setResponse = client.Set("Students/" + data.Id, data);
+
+                if (setResponse.StatusCode == System.Net.HttpStatusCode.OK)
+                {
+                    ModelState.AddModelError(string.Empty, "Added Successfully");
+                }
+                else
+                {
+                    ModelState.AddModelError(string.Empty, "Something went wrong!!");
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError(string.Empty, ex.Message);
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Edit(string id)
+        {
+            client = new FireSharp.FirebaseClient(config);
+            FirebaseResponse response = client.Get("Students/" + id);
+            Student data = JsonConvert.DeserializeObject<Student>(response.Body);
+            return View(data);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Student student)
+        {
+            client = new FireSharp.FirebaseClient(config);
+            SetResponse response = client.Set("Students/" + student.Student_id, student);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(string id)
+        {
+            client = new FireSharp.FirebaseClient(config);
+            FirebaseResponse response = client.Delete("Students/" + id);
+            return RedirectToAction("Index");
+        }
+
+    }
+}
